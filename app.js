427f42b2af32e8b8740253347078fbaf9c8d128c
Carsten Gross <carstengross1986@googlemail.com>
2015-03-16 14:21:39 +0100

    Serverseite

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
+var rc = require('piswitch');
+
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     conf = require('./config.json');
 
+// PiSwitch einrichten
+rc.setup({
+    mode: 'phys',
+    pulseLength: 330,
+    protocol: 1,
+    pin: 11
+});
+
+//Server starten
 server.listen(conf.port);
 
-app.use(express.static(path.join(__dirname, 'public')));
+//Pfad
+app.use(express.static(path.join(dirname, 'public')));
 
+//
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/public/index.html');
 });
 
+//
 io.sockets.on('connection', function(socket) {
-
+    socket.on('switch_control', function (data) {
+        //schalten der Steckdosen
+        // Code 1111110000, Typ-Dipschalter, an(false)/aus(true)
+        rc.send(data.code, dip, data.status);
+        console.log("[SEND] " + data.code +" "+ data.status);
+    })
 });
