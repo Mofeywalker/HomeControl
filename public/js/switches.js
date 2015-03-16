dbc4ecce20b9ba8c98106d50dfbf665b466fd403
mofeywalker <moritz.fey@gmail.com>
2015-03-16 14:19:15 +0100

    update der socket io nachrichten

--- public/js/switches.js
+++ public/js/switches.js
@@ -1,4 +1,7 @@
+var socket;
+
 $(document).ready(function() {
+    socket = io.connect();
     $.ajax({
         url: "switches.json",
         dataType: "text",
@@ -7,13 +10,21 @@ $(document).ready(function() {
             $.each(json.switches, function (index, value) {
                 $("#switches").append(
                     '<div class="col-md-6 col-xs-12" align="center">'
-                + '<div class="switch" class=col-md-12 col-xs-12 align="center">'
+                + '<div class="switch" class="col-md-12 col-xs-12" align="center">'
                 + '<h2>' + value.id + '</h2>'
                 + '</div>'
                 + '<button type="button" class="btn btn-success" id=' + value.id.toLowerCase() + 'an' + '">An</button>'
                 + '<button type="button" class="btn btn-danger" id=' + value.id.toLowerCase() + 'aus' + '">Aus</button>'
                 + '</div>'
-                )
+                );
+
+                $('#'+value.id.toLowerCase()+'an').click(function() {
+                    socket.emit('switch_control', {type: 'request', code: value.code, status:'false'});
+                });
+
+                $('#'+value.id.toLowerCase()+'aus').click(function() {
+                    socket.emit('switch_control', {type: 'request', code: value.code, status:'true'});
+                });
             });
         }
     });
