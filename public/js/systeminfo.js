92799fd478d4e36584083332153b97d45adfb14f
mofeywalker <moritz.fey@gmail.com>
2015-03-16 17:51:41 +0100

    Anpassungen für Systeminfo.js

--- public/js/systeminfo.js
+++ public/js/systeminfo.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
         $("#ostype").text(data.ostype);
         $("#osplat").text(data.osplat);
         $("#release").text(data.release);
-        $("#uptime").text(data.uptime);
-        $("#freemem").text(data.freemem);
+        $("#uptime").text((data.uptime / 60) + " min");
+        $("#freemem").text((data.freemem /1024 /1024) + " MB");
     });
 
     socket.emit('sysinfo', {});
