55f346a9046447538df1651f50a81f7608c9bbf4
mofeywalker <moritz.fey@gmail.com>
2015-03-16 11:06:09 +0100

    Weiter an der Boilderplate

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express'),
     io = require('socket.io').listen(server),
     conf = require('./config.json');
 
+server.listen(conf.port);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -11,5 +12,6 @@ app.get('/', function(req, res) {
     res.sendfile(__dirname + '/public/index.html');
 });
 
+io.sockets.on('connection', function(socket) {
 
-server.listen(conf.port);
+});
