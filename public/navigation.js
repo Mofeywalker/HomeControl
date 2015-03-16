6226f4383bf6bedac410debce832027f4f1bd07d
informaniac <osakasan@abwesend.de>
2015-03-16 14:42:35 +0100

    navigation funktioniert

--- public/navigation.js
+++ public/navigation.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
     $("#content").load("dashboard.html");
-    $('.navbar').click(function(e){
         //e.preventDefault();
-        var cont = $(this).data('cont');
-        switch(cont){
-            case "navDashboard":
-                $("#content").load("dashboard.html");
-                break;
-            case "navFernbedienung":
-                $("#content").load("switch.html");
-                break;
-            case "navSysinfo":
-                $("#content").load("sysinfo.html");
-                break;
-            default: break;
-        }
-    });
+
 });
+
+function navLoad($what){
+
+    switch($what){
+        case "navDashboard":
+            $("#content").load("dashboard.html");
+            break;
+        case "navSwitch":
+            $("#content").load("switch.html");
+            break;
+        case "navSysinfo":
+            $("#content").load("sysinfo.html");
+            break;
+        default: break;
+    }
+}
