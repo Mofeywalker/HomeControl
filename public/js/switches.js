d93e78a9983f2210d6c153ea2114585e384bc530
mofeywalker <moritz.fey@gmail.com>
2015-03-16 13:58:04 +0100

    seite für die switches

---
+++ public/js/switches.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "switches.json",
+        dataType: "text",
+        success: function (data) {
+            var json = $.parseJSON(data);
+            $.each(json.switches, function (index, value) {
+                $("#switches").append(
+                    '<div class="col-md-6 col-xs-12" align="center">'
+                + '<div class="switch" class=col-md-12 col-xs-12 align="center">'
+                + '<h2>' + value.id + '</h2>'
+                + '</div>'
+                + '<button type="button" class="btn btn-success" id=' + value.id.toLowerCase() + 'an' + '">An</button>'
+                + '<button type="button" class="btn btn-danger" id=' + value.id.toLowerCase() + 'aus' + '">Aus</button>'
+                + '</div>'
+                )
+            });
+        }
+    });
+});
+
