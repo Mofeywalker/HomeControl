b3d5d173820a9778cc4915562c1cfe88c671e3b8
Carsten Gross <carstengross1986@googlemail.com>
2015-03-19 14:42:48 +0100

    Cam Test 1

---
+++ public/js/camera.js
@@ -0,0 +1,11 @@
+var socket;
+$(document).ready(function() {
+    socket = io.connect();
+
+    socket.on('camera', function(data) {
+        console.log(data);
+    });
+
+    socket.emit('camera', {});
+
+});
