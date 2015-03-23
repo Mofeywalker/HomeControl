1c9fa13c143e378efd4d4b676f061facce865f59
Carsten Gross <carstengross1986@googlemail.com>
2015-03-23 17:40:24 +0100

    Temp Live Werte

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -30,12 +30,9 @@ $(document).ready(function() {
             renderTo: 'tempVerlauf',
             defaultSeriesType: 'spline',
             backgroundColor: 'rgba(20,20,20,0.1)',
-            type: 'spline',
-            animation: Highcharts.svg, // don't animate in old IE
-            marginRight: 10,
             events: {
-                load: function () {
-
+                load: function() {
+                    // Each time you receive a value from the socket, I put it on the graph
                     socket.on('temperatureUpdate', function (time, data) {
                         var series = chart.series[0];
                         series.addPoint([time, data]);
@@ -44,50 +41,29 @@ $(document).ready(function() {
             }
         },
         title: {
-            text: 'Live random data'
+            color: '#fbfbfb',
+            text: 'Temperatur'
+        },
+        scrollbar: {
+            enabled: true
         },
         xAxis: {
             type: 'datetime',
-            tickPixelInterval: 150
+            tickPixelInterval: 150,
+            maxZoom: 20 * 1000
         },
         yAxis: {
+            minPadding: 0.2,
+            maxPadding: 0.2,
             title: {
-                text: 'Value'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            formatter: function () {
-                return '<b>' + this.series.name + '</b><br/>' +
-                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-                    Highcharts.numberFormat(this.y, 2);
+                color: '#fbfbfb',
+                text: 'Temperatur ºC',
+                margin: 10
             }
-        },
-        legend: {
-            enabled: false
-        },
-        exporting: {
-            enabled: false
         },
         series: [{
             name: 'Temperatur',
-            data: (function () {
-                // generate an array of random data
-                var data = [],
-                    time = (new Date()).getTime(),
-                    i;
-
-                for (i = -19; i <= 0; i += 1) {
-                    data.push({
-                        x: time + i
-                    });
-                }
-                return data;
-            }())
+            data: []
         }]
     });
 
