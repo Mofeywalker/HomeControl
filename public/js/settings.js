027f29a6662d489c613d64e2686320482746d927
mofeywalker <moritz.fey@gmail.com>
2015-03-23 13:57:38 +0100

    Fehler

--- public/js/settings.js
+++ public/js/settings.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var socket = io.connect();
     socket.emit('temp_sensors_request', {});
     socket.on('temp_sensors_response', function(data) {
-        var json = parseJSON(data);
+        var json = $.parseJSON(data);
         $.each(json, function(index, value) {
             $('#tempsensor').enable();
             $('#tempsensor').append('<option>'+ value + '</option>');
