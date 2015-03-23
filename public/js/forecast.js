db0b740a8ec044aa09b6624ef74790a48de8fea0
Julian <juliankirsch1991@gmail.com>
2015-03-23 15:29:27 +0100

    wetter im dashboard

--- public/js/forecast.js
+++ public/js/forecast.js
@@ -1,21 +1,33 @@
 $(document).ready(function() {
-    var weather = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=Illingen,de&units=metric&lang=de';
-    var wetterart;
+    console.log("Forecast document ready");
     $.ajax({
         dataType: "jsonp",
-        url: weather,
+        url: 'http://api.openweathermap.org/data/2.5/forecast/daily?q=Illingen,de&units=metric&lang=de&cnt=4',
         success: function(data) {
-            var edit_save;
-
-
-            for (var i = 1; i <= 4; i++){
-                wetterart = data.list[i-1].weather[0].icon
-                edit_save = document.getElementById("tag" + i);
-                edit_save.src = "http://openweathermap.org/img/w/" + wetterart + ".png"
-
-                document.getElementById("temp_tag" + i).innerHTML = '<h3>'  + data.list[i-1].temp.day + " &deg;C  " + '</h3>';
-                document.getElementById("day_tag" + i).innerHTML = '<h3>' + $.datepicker.formatDate('dd.mm.yy', new Date (new Date(data.list[i-1].dt*1000))) + '</h3>';
-            }
+            console.log(data);
+            //var json = $.parseJSON(data);
+            //console.log(json);
+            $.each(data.list, function (index, value) {
+                var datum = new Date(value.dt*1000);
+                var tag = datum.getDate();
+                var monat = datum.getMonth();
+                monat++;
+                if(tag < 10){
+                    tag = '0'+tag;
+                }
+                if(monat < 10){
+                    monat = '0'+monat;
+                }
+                var datumtext = tag+'.'+monat+'.';
+                console.log(value.dt);
+                $("#forecast").append(
+                    '<div class="col-md-3 col-xs-12">'
+                     + datumtext + '<br>'
+                     +'<img src="http://openweathermap.org/img/w/'+value.weather[0].icon+'.png">'
+                     + value.temp.day + " &deg;C  "
+                     +'</div>'
+                )
+            });
         }
     });
 });
