632bd080d5e3b5b744acaeecbc0a53d56ee9473b
Carsten Gross <carstengross1986@googlemail.com>
2015-03-23 11:24:02 +0100

    Camera cache

--- public/js/camera.js
+++ public/js/camera.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
     socket.on('camera', function(data) {
         console.log(data);
         var d = new Date();
-        document.getElementById("camPic").innerHTML="<img id='camPic' src='camPics/camPic.jpg'>";
         document.getElementById("camPic").src = "camPics/camPic.jpg?ver="+d.getTime();
     });
 
