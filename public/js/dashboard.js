0a073f27ebc7d9f7110aab5993a7ccd52a66f17d
Carsten Gross <carstengross1986@googlemail.com>
2015-03-23 13:18:43 +0100

    TempUpdate done

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,45 +1,6 @@
 var socket;
 var chart;
 
-chart = new Highcharts.Chart({
-    chart: {
-        renderTo: 'chart',
-        defaultSeriesType: 'spline',
-        events: {
-            load: function() {
-                // Each time you receive a value from the socket, I put it on the graph
-                socket.on('temperatureUpdate', function (time, data) {
-                    var series = chart.series[0];
-                    series.addPoint([time, data]);
-                });
-            }
-        }
-    },
-    rangeSelector : {
-        selected : 100
-    },
-    title: {
-        text: 'Temperature Wohnzimmer'
-    },
-    xAxis: {
-        type: 'datetime',
-        tickPixelInterval: 150,
-        maxZoom: 20 * 1000
-    },
-    yAxis: {
-        minPadding: 0.2,
-        maxPadding: 0.2,
-        title: {
-            text: 'Temperature ºC',
-            margin: 80
-        }
-    },
-    series: [{
-        name: 'Temperature',
-        data: []
-    }]
-});
-
 $(document).ready(function() {
     setInterval(writeDateTime, 1000);
 
@@ -55,5 +16,44 @@ $(document).ready(function() {
     setInterval(function() {
         socket.emit('tempsensor',{});
     }, 10000);
+
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart',
+            defaultSeriesType: 'spline',
+            events: {
+                load: function() {
+                    // Each time you receive a value from the socket, I put it on the graph
+                    socket.on('temperatureUpdate', function (time, data) {
+                        var series = chart.series[0];
+                        series.addPoint([time, data]);
+                    });
+                }
+            }
+        },
+        rangeSelector : {
+            selected : 100
+        },
+        title: {
+            text: 'Temperature Wohnzimmer'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150,
+            maxZoom: 20 * 1000
+        },
+        yAxis: {
+            minPadding: 0.2,
+            maxPadding: 0.2,
+            title: {
+                text: 'Temperature ºC',
+                margin: 80
+            }
+        },
+        series: [{
+            name: 'Temperature',
+            data: []
+        }]
+    });
 
 });
