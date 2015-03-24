03666ab64411a18dc26a60c193cc2eff30793d9d
Mofeywalker <moritz.fey@gmail.com>
2015-03-24 22:05:13 +0100

    Steckdosen werden jetzt aus der Datenbank gelesen

--- public/js/switches.js
+++ public/js/switches.js
@@ -2,42 +2,49 @@ var socket;
 
 $(document).ready(function() {
     socket = io.connect();
-    $.ajax({
-        url: "switches.json",
-        dataType: "text",
-        success: function (data) {
-            var json = $.parseJSON(data);
-            $.each(json.switches, function (index, value) {
-                $("#switches").append(
-                    '<div class="col-md-6 col-xs-12" align="center">'
+    socket.on('switch_all_response', function(switches) {
+        //var json = $.parseJSON(data);
+        console.log(switches);
+        $.each(switches, function (index, value) {
+            $("#switches").append(
+                '<div class="col-md-6 col-xs-12" align="center">'
                 + '<div class="switch" class="col-md-12 col-xs-12" align="center">'
-                + '<h2>' + value.id + '</h2>'
+                + '<h2>' + value.name + '</h2>'
                 + '</div>'
-                + '<button type="button" class="btn btn-on" id="' + value.id.toLowerCase() + 'an"' + '">An</button>'
-                + '<button type="button" class="btn btn-off" id="' + value.id.toLowerCase() + 'aus"' + '">Aus</button>'
+                + '<button type="button" class="btn btn-on" onclick="switchOn(' + value.code + ')' + '">An</button>'
+                + '<button type="button" class="btn btn-off" onclick="switchOff(' + value.code + ')' + '">Aus</button>'
                 + '</div>'
-                );
+            );
 
+        });
+
+        console.log($("#switches"));
+
+        /*
+        $.each(switches, function(index, value) {
+            var onId = '#'+value.id.toLowerCase()+'an';
+            console.log("[an]"+ onId);
+            $(onId).click(function() {
+
+                socket.emit('switch_control', {type: 'request', code: value.code, status:false});
             });
-
-            console.log($("#switches"));
-
-            $.each(json.switches, function(index, value) {
-                var onId = '#'+value.id.toLowerCase()+'an';
-                console.log("[an]"+ onId);
-                $(onId).click(function() {
-
-                    socket.emit('switch_control', {type: 'request', code: value.code, status:false});
-                });
-                var offId = '#'+value.id.toLowerCase()+'aus';
-                console.log("[an]"+ offId);
-                $(offId).click(function() {
-                    console.log("[aus]");
-                    socket.emit('switch_control', {type: 'request', code: value.code, status:true});
-                });
+            var offId = '#'+value.id.toLowerCase()+'aus';
+            console.log("[an]"+ offId);
+            $(offId).click(function() {
+                console.log("[aus]");
+                socket.emit('switch_control', {type: 'request', code: value.code, status:true});
             });
-
-        }
+        });
+        */
     });
+
+    socket.emit('switch_all_request');
 });
 
+function switchOn(code) {
+    socket.emit('switch_control', {type: 'request', code: code.toString(), status:false});
+}
+
+function switchOff(code) {
+    socket.emit('switch_control', {type: 'request', code: code.toString(), status:true});
+}
