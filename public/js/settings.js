219ff6254cb9ccc38eda7566f75325724130ef91
Mofeywalker <moritz.fey@gmail.com>
2015-03-24 15:01:04 +0100

    Fixes für falsche Darstellung in den Settings

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,16 +1,16 @@
-var socket;
-var cityName;
 $(document).ready(function() {
+    var socket;
     socket = io.connect();
 
     socket.on('switch_all_response', function(response){
         console.log(response);
         $("#steckdosen-liste").append('<table border="0">');
         $.each(response, function(index, data){
-            var str = "<tr><td>" + data.name + '' +
-                +'</td><td>('+ data.code +')</td><td><button class="change" onclick="changeButton('+data._id
-                +')">&Auml;ndern</button></td><td><button class="delete" onclick="deleteButton('+data._id
-                +')">L&ouml;schen</button></td></tr>';
+            var str = '<tr><td>' + data.name +
+                '</td><td>' + '('+ data.code +')' +
+                '</td><td><button class="change" onclick="changeButton('+data._id+')">&Auml;ndern</button>' +
+                '</td><td><button class="delete" onclick="deleteButton('+data._id+')">L&ouml;schen</button>' +
+                '</td></tr>';
             $("#steckdosen-liste").append(str);
         });
         $("#steckdosen-liste").append('</table>');
