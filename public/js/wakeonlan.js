4b7d79d5e45f65402f3f3a0629817c431113f98e
Mofeywalker <moritz.fey@gmail.com>
2015-03-29 13:32:52 +0200

    Kommentare für wakeonlan.js

--- public/js/wakeonlan.js
+++ public/js/wakeonlan.js
@@ -1,8 +1,12 @@
-
-
 $(document).ready(function() {
+    // Mit Websocket verbinden
     var socket;
     socket = io.connect();
+
+    /**
+     * Socket-Listener der auf eine eingehende Antwort wartet, die alle verfuegbaren Wake On Lan Eintrage beinhaltet
+     * und die Seite dynamisch aufbaut.
+     */
     socket.on('wol_all_response_overview', function(wols) {
         console.log(wols);
 
@@ -19,41 +23,14 @@ $(document).ready(function() {
         });
     });
 
-    /*
-
-    $.ajax({
-        url: "wakeonlan.json",
-        dataType: "text",
-        success: function (data) {
-            var json = $.parseJSON(data);
-            $.each(json.wakeonlan, function (index, value) {
-                $("#wakeonlan").append(
-                    '<div class="col-md-6 col-xs-12" align="center">'
-                + '<div class="switch" class="col-md-12 col-xs-12" align="center">'
-                + '<h2>' + value.name + '</h2>'
-                + '</div>'
-                + '<button type="button" class="btn btn-on" id="' + value.id.toLowerCase() + 'an"' + '">An</button>'
-                + '</div>'
-                );
-
-            });
-
-            console.log($("#wakeonlan"));
-
-            $.each(json.wakeonlan, function(index, value) {
-                var onId = '#'+value.id.toLowerCase()+'an';
-                console.log("[an]"+ onId);
-                $(onId).click(function() {
-                    socket.emit('wakeonlan_control', {id: value.id , mac: value.mac});
-                });
-            });
-
-        }
-    });
-    */
+    // Anfrage fuer alle WOL Eintraege
     socket.emit('wol_all_request', {type: 'overview'});
 });
 
+/**
+ * Funktion zum senden einer WOl Anfrage an den Server.
+ * @param index
+ */
 function switchOn(index) {
     var mac = $("#wake"+index).text();
     socket.emit('wakeonlan_control', {mac:mac.toString()});
