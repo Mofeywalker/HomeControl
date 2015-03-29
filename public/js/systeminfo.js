a974510b12efad81eec6d0318f64b43fbd6d5c06
Mofeywalker <moritz.fey@gmail.com>
2015-03-29 13:30:46 +0200

    Kommentare für systeminfo.js

--- public/js/systeminfo.js
+++ public/js/systeminfo.js
@@ -1,7 +1,12 @@
-var socket;
 $(document).ready(function() {
+    // Mit Websocket verbinden
+    var socket;
     socket = io.connect();
 
+    /**
+     * Socket-Listener der auf eine sysinfo Antwort wartet. Es werden verschiedenste Systeminfos auf der Seite
+     * eingefuegt.
+     */
     socket.on('sysinfo', function(data) {
         console.log(data);
         $("#hostname").text(data.hostname);
@@ -12,8 +17,10 @@ $(document).ready(function() {
         $("#freemem").text((Math.round((data.freemem /1024 /1024)*1000)/1000) + " MB");
     });
 
+    // Systeminfos beim Server anfragen, wird nur beim ersten Mal aufgerufen
     socket.emit('sysinfo', {});
 
+    // Alle 5 Sekunden werden die Infos auf der Seite aktualisiert
     setInterval(function() {
         socket.emit('sysinfo', {});
     }, 5000);
