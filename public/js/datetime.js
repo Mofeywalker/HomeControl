12290bfe8fdc220a4751924caae693965d5d47b2
informaniac <osakasan@abwesend.de>
2015-03-29 18:32:11 +0200

    datetime.js kommentiert

--- public/js/datetime.js
+++ public/js/datetime.js
@@ -1,15 +1,24 @@
+/*
+ * writeDateTime
+ * Funktion um Datum und Zeit an Ihren Platz auf dem Dashboard zu schreiben
+ */
 function writeDateTime(){
+    // Neues Zeitobjekt erzeugen
     var dt = new Date;
 
+    // Tag, Monat und Jahr auslesen
     var d = dt.getDate();
     var m = dt.getMonth();
+    // Monat wird von 0-11 zurückgegeben, repräsentativ für Monate 1-12, daher Inkrement
     m++;
     var y = dt.getFullYear();
 
+    // Stunden, Minuten und Sekunden auslesen
     var hh = dt.getHours();
     var mm = dt.getMinutes();
     var ss = dt.getSeconds();
 
+    // Führende Nullen hinzufügen, falls nötig
     if(m < 10){
         m = "0"+m;
     }
@@ -23,8 +32,10 @@ function writeDateTime(){
         ss = "0"+ss;
     }
 
+    // Ergebnis-String erzeugen
     res = d+'.'+m+'.'+y+"<br>"+hh+':'+mm+':'+ss;
 
+    // Einbinden des Zeit-String
     try{
         document.getElementById("date").innerHTML = res;
     }
