36e6e8628d0ac1fbb3c8adb7836af1cf3784af09
Mofeywalker <moritz.fey@gmail.com>
2015-03-29 13:21:29 +0200

    Kommentare für camera.js

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,13 +1,17 @@
-var socket;
 $(document).ready(function() {
+    // Mit dem Websocket verbinden
+    var socket;
     socket = io.connect();
 
+    // Websocket-Listener, der auf eingehende Kameraaktualisierungen wartet
     socket.on('camera', function(data) {
         console.log(data);
         var d = new Date();
+        // Bild in die Website einbinden
         document.getElementById("camPic").src = "camPics/camPic.jpg?ver="+d.getTime();
     });
 
+    // Anfrage fuer ein neues Foto, wird beim refresh der Seite ausgefuehrt
     socket.emit('camera', {});
 
 });
