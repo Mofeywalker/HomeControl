f33271e316988ba499af2e58d4a3cd312aa850c8
Mofeywalker <moritz.fey@gmail.com>
2015-03-29 13:49:26 +0200

    Kommentare für weatherHandler.js

--- weatherHandler.js
+++ weatherHandler.js
@@ -1,14 +1,24 @@
-var nconf           = require('nconf');
+// Nconf zum auslesen der Config
+var nconf = require('nconf');
 
 // Config einlesen
 nconf.use('file', {file: './config.json'});
 nconf.load();
 
 module.exports = function(socket) {
+    /**
+     * Socket-Listener der auf ein Wetter-Update Request wartet und dann den Ort zurueck sendet,
+     * beim Client werden dann die neusten Wetter-Informationen von openweathermap abgerufen
+     */
     socket.on('weatherlocation_request', function() {
         socket.emit('weatherlocation_response', {weatherlocation: nconf.get('weatherlocation')});
     });
 
+
+    /**
+     * Socket-Listener der auf ein Update des Ortes wartet, fuer den das Wetter abgefragt wird.
+     * Diese Information wird in der config.json gepeichert.
+     */
     socket.on('weatherlocation_update', function(data) {
         nconf.set('weatherlocation', data.weatherlocation);
         nconf.save(function (err) {
@@ -16,7 +26,7 @@ module.exports = function(socket) {
                 console.error(err.message);
                 return;
             }
-            console.log('Configuration saved successfully.');
+            console.log('[NCONF - Standort fuer Wetterabfragen erfolgreich geaendert!]');
         });
     });
 };
